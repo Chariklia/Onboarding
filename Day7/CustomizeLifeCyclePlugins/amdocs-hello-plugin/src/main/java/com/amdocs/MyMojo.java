4b013a3df245fd5dd24172441917a3745de96224
Chariklia Economide <Chariklia.Economide@amdocs.com>
2019-10-03 15:39:13 +0300

    first commit

---
+++ Day7/CustomizeLifeCyclePlugins/amdocs-hello-plugin/src/main/java/com/amdocs/MyMojo.java
@@ -0,0 +1,21 @@
+package com.amdocs;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+
+/**
+ * Goal which prints Hello message
+ */
+
+@Mojo( name = "hello", defaultPhase = LifecyclePhase.PROCESS_SOURCES )
+public class MyMojo
+    extends AbstractMojo
+{
+    public void execute()  //where we write the code
+        throws MojoExecutionException
+    {
+	System.out.println("Amdocs Hello Custom Plugin!");
+    }
+}
